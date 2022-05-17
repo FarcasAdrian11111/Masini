174d00eba7bfd5873f894e077686de1d734b6121
Farcas Adrian <vest4est3@gmail.com>
2022-05-17 23:38:14 +0300

    ECM-7

--- src/main/java/org/loose/fis/sre/controllers/cumparareGUIController.java
+++ src/main/java/org/loose/fis/sre/controllers/cumparareGUIController.java
@@ -89,6 +89,6 @@ public class cumparareGUIController implements Initializable {
         myImages[4] = new Image(getClass().getClassLoader().getResource("fiat.jpg").toString(), true);
         myImages[5] = new Image(getClass().getClassLoader().getResource("bmw.jpg").toString(), true);
         myImages[6] = new Image(getClass().getClassLoader().getResource("volkswagen.jpg").toString(), true);
-        imagine.setImage(myImages[1]);
+        imagine.setImage(myImages[0]);
     }
 }
