1662f1ca5e582506a2057eb53e9a7263c0cfac13
Farcas Adrian <vest4est3@gmail.com>
2022-05-18 14:25:26 +0300

    ECM-7

--- src/main/java/org/loose/fis/sre/model/Cumparator.java
+++ src/main/java/org/loose/fis/sre/model/Cumparator.java
@@ -5,46 +5,72 @@ import org.dizitart.no2.objects.Id;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import org.loose.fis.sre.services.CumparatorService;
+
 public class Cumparator {
     @Id
     private String nume;
-    private ArrayList<Masina> preferinte;
+    private ArrayList<String> preferinte;
     private ArrayList<Masina> istoric;
     private ArrayList<String> mesaje;
 
-    public Cumparator(String nume){
-        this.nume=nume;
+    public Cumparator(String nume, ArrayList<String> preferinte, ArrayList<Masina> istoric, ArrayList<String> mesaje) {
+        this.nume = nume;
+        this.preferinte = preferinte;
+        this.istoric = istoric;
+        this.mesaje = mesaje;
     }
 
-    public String getNume(){
+    public Cumparator() {
+    }
+
+    public String getNume() {
         return this.nume;
     }
 
-    public void setNume(String nume){
-        this.nume=nume;
+    public void setNume(String nume) {
+        this.nume = nume;
     }
 
-    public void setPreferinte(ArrayList<Masina> preferinte){
-        this.preferinte=preferinte;
+    public void setPreferinte(ArrayList<String> preferinte) {
+        this.preferinte = preferinte;
     }
 
-    public ArrayList<Masina> getPreferinte(){
+    public ArrayList<String> getPreferinte() {
         return this.preferinte;
     }
 
-    public void setIstoric(ArrayList<Masina> istoric){
-        this.istoric=istoric;
+    public void setIstoric(ArrayList<Masina> istoric) {
+        this.istoric = istoric;
     }
 
-    public ArrayList<Masina> getIstoric(){
+    public ArrayList<Masina> getIstoric() {
         return this.istoric;
     }
 
-    public void setMesaje(ArrayList<String> mesaje){
-        this.mesaje=mesaje;
+    public void setMesaje(ArrayList<String> mesaje) {
+        this.mesaje = mesaje;
     }
 
-    public ArrayList<String> getMesaje(){
+    public ArrayList<String> getMesaje() {
         return this.mesaje;
+    }
+
+    public void addInPreferinte(String preferinta){
+        this.preferinte.add(preferinta);
+        CumparatorService.updateazaDBPreferinte(this.nume, this.preferinte);
+    }
+
+    public void addInIstoric(Masina history){
+        this.istoric.add(history);
+        CumparatorService.updateazaDBIstoric(this.nume, this.istoric);
+    }
+
+    public void addInMesaje(String mesaj){
+        this.mesaje.add(mesaj);
+        /*for(String mesajul : this.mesaje){
+            System.out.println(mesajul);
+        }*/
+        CumparatorService.updateazaDBMesaje(this.nume, this.mesaje);
     }
 }
