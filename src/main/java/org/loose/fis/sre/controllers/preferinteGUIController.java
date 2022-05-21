5f5e4853d76fc364e3c82f4dea2b9845474f3e2e
Farcas Adrian <vest4est3@gmail.com>
2022-05-21 17:51:01 +0300

    ECM-8

--- src/main/java/org/loose/fis/sre/controllers/preferinteGUIController.java
+++ src/main/java/org/loose/fis/sre/controllers/preferinteGUIController.java
@@ -14,7 +14,11 @@ import org.loose.fis.sre.exceptions.InvalidCredentialException;
 import org.loose.fis.sre.services.MasinaService;
 import org.loose.fis.sre.model.Masina;
 import org.loose.fis.sre.services.CumparatorService;
+import org.loose.fis.sre.services.VanzatorService;
+import org.loose.fis.sre.services.OfertaService;
 import org.loose.fis.sre.model.Cumparator;
+import org.loose.fis.sre.model.Vanzator;
+import org.loose.fis.sre.model.mdc;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -43,6 +47,8 @@ import java.nio.file.Path;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import org.loose.fis.sre.exceptions.masinaInvalidaException;
+
 
 public class preferinteGUIController implements Initializable {
     @FXML
@@ -63,6 +69,41 @@ public class preferinteGUIController implements Initializable {
         //se executa prima, chiar inainte de getBuyer, deci poate ca buyer sa fie pe null initial
         //myListView.getItems().addAll(buyer.getPreferinte());
         //ce se intampla cand selectam si pretul e deja introdus => se trimite la vanzator sa se ocupe de el.
+        myListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
+                String[] sequence = ((String)myListView.getSelectionModel().getSelectedItem()).split(" ");
+                String marca = sequence[1];
+                String vanzator = sequence[sequence.length - 1];
+                VanzatorService.initDatabase();//nu mai poti selecta mai multe oferte pentru ca baza de date e deschisa deja la selectia initiala;
+                Vanzator seller = VanzatorService.getVanzator(vanzator);
+                //System.out.println(seller.getNume());
+                //introducere in DB Oferte
+                //
+                int i=-1;
+                for(mdc mdc2 : seller.getObjw()) {
+                    i++;
+                    if (mdc2.getMasina().getMarca().equals(marca)) {
+                        //System.out.println(mdc2.getDeVanzare());
+                        try {
+                            if (mdc2.getDeVanzare()) {
+                                mdc2.getBuyers().add(CumparatorService.getCumparator(buyer.getNume()));
+                                VanzatorService.updateazaDB_Objw(seller);
+                                OfertaService.initDatabase();
+                                System.out.println(buyer.getNume() + seller.getNume());
+                                OfertaService.insereaza(buyer, seller, Integer.parseInt(myTextField.getText()), marca);
+                                myText.setText("Ati introdus oferta!");
+                            } else {
+                                throw new masinaInvalidaException();
+                            }
+                            break;
+                        } catch (masinaInvalidaException ex){
+                            myText.setText(ex.getMessage());
+                        }
+                    }
+                }
+            }
+        });
     }
 
     public void getBuyerT(Cumparator buyer){
